9577aad1f0e956d37d99e5d32eec3fc15b9646ad
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-01-20 16:37:32 -0800

    less json and more strings

--- scripts/get-connected-devices.sh
+++ scripts/get-connected-devices.sh
@@ -11,4 +11,4 @@ DEVICES_JSON=$(cd /eero-client; ./sample.py devices)
 
 (echo "${DEVICES_JSON}" | jq empty >/dev/null 2>&1) || { echo -e "${DEVICES_JSON}"; exit 1; }
 
-echo -e "${DEVICES_JSON}" | jq '.[]' | jq -s | jq -r --arg NETWORK_FILTER "/${NETWORK_ID}/" '[.[] | select(.url | contains($NETWORK_FILTER)) | select(.connected == true) | {name: (if .nickname != null then .nickname else .hostname end) }]'
+echo -e "${DEVICES_JSON}" | jq '.[]' | jq -s | jq -r --arg NETWORK_FILTER "/${NETWORK_ID}/" '.[] | select(.url | contains($NETWORK_FILTER)) | select(.connected == true) | {name: (if .nickname != null then .nickname else .hostname end) } | .[]' | sort
--- scripts/is-someone-home.sh
+++ scripts/is-someone-home.sh
@@ -4,8 +4,6 @@ cd "$(dirname "$0")"
 
 CONNECTED_DEVICES_JSON=$(./get-connected-devices.sh)
 
-(echo "${CONNECTED_DEVICES_JSON}" | jq empty >/dev/null 2>&1) || { echo -e "${CONNECTED_DEVICES_JSON}"; exit 1; }
+SEARCH_CONNECTED_COUNT=$(echo -e "${CONNECTED_DEVICES_JSON}" | grep -E "${SEARCH_CONNECTED_REGEXP}" | wc -l)
 
-SEARCH_CONNECTED=$(echo -e "${CONNECTED_DEVICES_JSON}" | jq --arg SEARCH_CONNECTED_REGEXP "${SEARCH_CONNECTED_REGEXP}" '[.[] | select(.name | test($SEARCH_CONNECTED_REGEXP))] | length')
-
-echo $SEARCH_CONNECTED
+echo $SEARCH_CONNECTED_COUNT
