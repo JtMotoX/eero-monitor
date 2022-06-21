6cb5f7f0b3efc9f00b82184f9f1f21a8e4f10abd
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-06-20 18:25:55 -0700

    improve 1Password error handling

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,9 +2,9 @@
 
 # CONVERT 1PASSWORD ENVIRONMENT VARIABLES
 if env | grep -E '^[^=]*=OP:' >/dev/null; then
-	curl -s -o /tmp/1password-vars.sh "https://raw.githubusercontent.com/JtMotoX/1password-docker/main/1password/op-vars.sh"
+	curl -sS -o /tmp/1password-vars.sh "https://raw.githubusercontent.com/JtMotoX/1password-docker/main/1password/op-vars.sh"
 	chmod 755 /tmp/1password-vars.sh
-	. /tmp/1password-vars.sh
+	. /tmp/1password-vars.sh || exit 1
 	rm -f /tmp/1password-vars.sh
 fi
 
